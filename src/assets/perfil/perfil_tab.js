60939059d35c88569c192a0658961dee262bdf9e
Alejandro Lindarte Castro <alejo.lindarte@outlook.com>
2018-05-11 16:13:02 -0500

    Part 2

---
+++ src/assets/perfil/perfil_tab.js
@@ -0,0 +1,9 @@
+$(document).ready(function() 
+{
+	$(".btn-pref .btn").on('click',function () 
+	{		
+		$(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
+    // $(".tab").addClass("active"); // instead of this do the below 
+		$(this).removeClass("btn-default").addClass("btn-primary");   
+	});
+});
